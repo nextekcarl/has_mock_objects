55de3b1e80ec13beab0d837e1718bf01097572f5
Carl Anderson <carl@planetargon.com>
2013-11-27 09:13:05 -0800

    Added tests.

--- test/has_mock_objects_test.rb
+++ test/has_mock_objects_test.rb
@@ -4,4 +4,31 @@ class HasMockObjectsTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, HasMockObjects
   end
+
+  def test_a_books_mock_chapters_should_return_array
+    book = Book.new
+    assert_equal true, book.chapters.is_a?(Array)
+  end
+
+  def test_a_books_mock_chapters_should_count_should_vary_between_instances
+    books = []
+    10.times do
+      books << Book.new
+    end
+    chapter_counts =[]
+    books.each do |book|
+      chapter_counts << book.chapters.count
+    end
+    assert_equal true, chapter_counts.uniq.size != 1
+  end
+
+  def test_a_books_mock_author_should_return_one_author_with_an_email
+    book = Book.new
+    assert_equal true, book.author.email.is_a?(String)
+  end
+
+  def test_a_books_mock_author_email_should_not_be_regenerated_after_first_call
+    book = Book.new
+    assert_equal true, book.author.email == book.author.email
+  end
 end
